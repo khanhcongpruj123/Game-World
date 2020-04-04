e255ff732173e3a694f4eb99cbb53f89be994cdc
Tran Duy Cong Khanh <khanhcongpruj123@gmail.com>
2020-04-04 15:14:33 +0700

    add get detail game usecase

--- data/src/main/java/com/icongkhanh/gameworld/data/remote/GameService.kt
+++ data/src/main/java/com/icongkhanh/gameworld/data/remote/GameService.kt
@@ -2,10 +2,12 @@ package com.icongkhanh.gameworld.data.remote
 
 import com.icongkhanh.gameworld.data.remote.di.HOST
 import com.icongkhanh.gameworld.data.remote.di.KEY
+import com.icongkhanh.gameworld.data.remote.model.GameResponse
 import com.icongkhanh.gameworld.data.remote.model.GetAllGameResponse
 import com.icongkhanh.gameworld.domain.model.Game
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GameService {
@@ -16,4 +18,11 @@ interface GameService {
     )
     @GET("/games")
     suspend fun getAllGame(@Query("page") page: Long = 1): GetAllGameResponse
+
+    @Headers(
+        "x-rapidapi-host: ${HOST}",
+        "x-rapidapi-key: ${KEY}"
+    )
+    @GET("/games/{game_pk}")
+    suspend fun getGameDetail(@Path("game_pk") gameId: Long): GameResponse
 }
